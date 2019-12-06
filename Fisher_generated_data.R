6ad06479248e0d8379ba39d85235212cf4a4fd27
Gabrielė Stravinskaitė <52568826+GStravinsky@users.noreply.github.com>
2019-12-06 12:49:17 +0000

    Update Fisher_generated_data.R
    Includes Tkhonov's regularisation.

--- Fisher_generated_data.R
+++ Fisher_generated_data.R
@@ -6,47 +6,54 @@ X <- cbind(X, c(1,1,1,1,1,1,1,1,1,1))
 r <- ncol(X) - 1 # number of regression coefficients - 1
 beta_new <- c(log(sum(y) / sum(m - y)), rep(0, r))
 
-
 # Define logistic regression
 logistic <- function(X, beta) {
-  p <- exp(X %*% beta)/(1+exp(X%*%beta))
+  p <- exp(X %*% beta)/(1+exp(X %*% beta))
 }
 
-Fisher_scoring <- function(X, y, m, beta_new, maxiter, delta.beta) {
-  
-  # beta2 is used instrumentally to generate comparisons and calculate differences
+Fisher_scoring <- function(X, y, m, beta_new, maxiter, delta.betas) {
+  # X - a matrix of covariates, must contain a column of 1s as an intercept
+  # y - a vector of "successes" 
+  # m - a vector denoting the total number of trials - equals to 1 for Bernoulli.
+  #  m is equal to 1 as in 2a). 
+  # beta_new - a vector of initial guess for betas
+  # maxiter - a bound for a maximum number of iterations
+  # delta.beta - the bound for difference between the consecutive 
+  approximated betas that indicates convergence
+ 
   beta_old <- rep(Inf, length(beta_new))
-  # Euclidian distance to calulate the improvements
-  diff.betas <- sqrt(sum(beta_new-beta_old)^2)
+  # Euclidian distance 
+  diff.betas <- sqrt(sum((beta_new - beta_old)^2))
   iter <- 1 
-  beta.hist <- matrix(beta_new, nrow=1)
   
-  while((iter<=iter) & (diff.betas>delta.beta)) {
+  while((iter<=maxiter) & (diff.betas>delta.betas)) {
     iter <- iter + 1
-    # because the original input beta1 is the initial guess, 
-    # and beta1 in the code bellow represents improved the approximation of beta 
     beta_old <- beta_new
     # estimate the improvement part of the alogrithm: (t(X)WX)^-1*t(X)(y-mp)
     W <- diag(as.vector(m*logistic(X, beta_old)*(1-logistic(X, beta_new))))
     mp <- m * logistic(X, beta_old)
-    # (t(X)WX)^-1*t(X)(y-mp) = Z, in R - solve t(X)WX * Z = t(X)(y-mp)
-    improvement <- solve(t(X) %*% W %*% X, t(X) %*% (y-mp))
+    # XWX is calculated with Tikhonov reguliarisation -- multicolinearity problem
+    XWX <- t(X) %*% W %*% X + diag(1, 58, 58) * 0.0001
+    Fscore <- t(X) %*% (y-mp)
+    # (XWX)^-1 * Fscore = Z, in R - solve XWX * Z = Fscore
+    improvement <- solve(XWX, Fscore)
     # the final algorithm of iterations
     beta_new <- beta_old + improvement
     
-    diff.betas <- sqrt(sum(beta_new-beta_old)^2)
-    beta.hist <- rbind(beta.hist, (matrix(beta_new, nrow=1)))
+    diff.betas <- sqrt(sum((beta_new - beta_old)^2))
   }
   
   result <- list()
   result$beta.MLE <- beta_new
-  result$beta.history <- beta.hist
   result$iterations <- iter - 1
   return(result)
 }
 
+set.seed(1620789)
 Fisher_scoring(X, y, m, beta_new, maxiter=50, delta.beta = 0.0001)
 
+# Comparison with the inbuilt model
 glm.fit <- glm( y~X, family = binomial)
 summary(glm.fit)
+# same result, slower convergence.
 
